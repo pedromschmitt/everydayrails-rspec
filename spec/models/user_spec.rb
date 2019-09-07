3ea958f6fdf9fdda21e50cd79059f1b266949e33
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-09-07 10:17:45 -0300

    Create rspec for first and last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,18 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
     
-  it "is invalid without a first name"
-  it "is invalid without a last name"
+  it "is invalid without a first name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
   it "is invalid without a email address"
   it "is invalid with a duplicate email address"
   it "returns a user's full name as a string"
