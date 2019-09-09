285a1d0473fc50bfd75937d7a408b8e8ea1da917
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-09-09 11:44:07 -0300

    Create test for full name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,5 +42,12 @@ RSpec.describe User, type: :model do
     expect(user.errors[:email]).to  include("has already been taken")
   end
 
-  it "returns a user's full name as a string"
+  it "returns a user's full name as a string" do
+    user = User.new(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "tester@example.com",
+    )
+    expect(user.name).to eq "Joe Tester"
+  end
 end
